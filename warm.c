56c47155714d0d8d8f200b6d98f762554571513e
RichNachos <gkldi20@freeuni.edu.ge>
2024-12-02 16:02:45 +0400

    Add read timeout

--- warm.c
+++ warm.c
@@ -14,14 +14,19 @@ void disableRawTerminalMode();
 int main(void) {
     enableRawTerminalMode();
 
-    char c;
-
     // Read until 'q' char or EOF
-    while(read(STDIN_FILENO, &c, 1) && c != QUIT_KEY) {
+    while(1) {
+        char c = '\0';
+        read(STDIN_FILENO, &c, 1);
+
         if (iscntrl(c)) {
             printf("%d\r\n", c);
         } else {
             printf("%d ('%c')\r\n", c, c);
+        }
+
+        if (c == QUIT_KEY) {
+            break;
         }
     }
     
@@ -41,6 +46,8 @@ void enableRawTerminalMode() {
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag &= ~(CS8);
     raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+    raw.c_cc[VMIN] = 0;
+    raw.c_cc[VTIME] = 1;
 
     tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
 }
