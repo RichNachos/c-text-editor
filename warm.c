aef6bfd8220fc47d6341c05a626426d6a559d2ee
RichNachos <gkldi20@freeuni.edu.ge>
2024-12-02 14:38:33 +0400

    Implement simple cat

--- warm.c
+++ warm.c
@@ -1,4 +1,11 @@
-int main(void) {
+#include <unistd.h>
 
+int main(void) {
+    char c;
+
+    while(read(STDIN_FILENO, &c, 1)) {
+        write(STDOUT_FILENO, &c, 1);
+    }
+    
     return 0;
 }
