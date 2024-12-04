f0af7091532c59178993cf9aa7a0a85230f6321e
RichNachos <gkldi20@freeuni.edu.ge>
2024-12-04 15:59:12 +0400

    Add line removal logic

--- warm.c
+++ warm.c
@@ -681,10 +681,16 @@ void editorInsertChar(int c) {
 
 void editorDeleteChar() {
     if (E.cursor_y == E.num_rows) return;
+    if (E.cursor_x == 0 && E.cursor_y == 0) return;
 
     editorRow* row = &E.row[E.cursor_y];
     if (E.cursor_x > 0) {
         editorRowDeleteChar(row, E.cursor_x - 1);
         E.cursor_x--;
+    } else {
+        E.cursor_x = E.row[E.cursor_y - 1].size;
+        editorRowAppendString(&E.row[E.cursor_y - 1], row->line, row->size);
+        editorDeleteRow(E.cursor_y);
+        E.cursor_y--;
     }
 }
